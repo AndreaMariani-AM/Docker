1185a4b4276bd8634c524cb7abd1cb2d8da9bbe7
AndreaMariani-AM <mariani.andrea1@live.it>
2021-11-30 12:11:12 +0100

    rnaseq dir is for now a test dir. Having problems with bioinfo softwares

---
+++ RNAseq-snakemake/building_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This is a small script to build the Docker image
+
+#set image version
+IMAGE_VERSION=$(date "+%m%d%y")
+
+# Build the image
+docker build --no-cache -t andreamariani/rnaseq:${IMAGE_VERSION} -t andreamariani/rnaseq:latest .
