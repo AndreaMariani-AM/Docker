620e99fc90631bdbaf3c3f9f33afec4fa16d6ef4
AndreaMariani-AM <mariani.andrea1@live.it>
2021-11-30 14:44:44 +0100

    now works, test if miniconda2 works too

---
+++ ChIPseq-snakemake/install_packages_cran.R
@@ -0,0 +1,9 @@
+# Cran packages
+
+install.packages(c(
+        "data.table",
+        "ggplot2",
+        "RColorBrewer",
+        "devtools",
+        "spp"),
+        repos="https://cloud.r-project.org/")
