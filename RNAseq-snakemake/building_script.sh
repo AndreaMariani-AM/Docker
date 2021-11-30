e57fbec54557716b884e4aa76f51bbef9cae5c5b
AndreaMariani-AM <mariani.andrea1@live.it>
2021-11-30 15:18:03 +0100

    trying to add other packages to see if brakes down

--- RNAseq-snakemake/building_script.sh
+++ RNAseq-snakemake/building_script.sh
@@ -6,4 +6,5 @@
 IMAGE_VERSION=$(date "+%m%d%y")
 
 # Build the image
-docker build --no-cache -t andreamariani/rnaseq:${IMAGE_VERSION} -t andreamariani/rnaseq:latest .
+docker build --no-cache -t andreamariani/rnaseq:${IMAGE_VERSION} -t andreamariani/rnaseq:latest . && \
+docker push andreamariani/rnaseq:${IMAGE_VERSION}
