eaa9603e8d415d99c4af46313c2014fcdc0769aa
AndreaMariani-AM <mariani.andrea1@live.it>
2021-12-01 09:42:26 +0100

    testing those packages that have conflicts

--- ChIPseq-snakemake/install_packages_cran.R
+++ ChIPseq-snakemake/install_packages_cran.R
@@ -2,8 +2,8 @@
 
 install.packages(c(
         "data.table",
-        # "ggplot2",
-        # "RColorBrewer",
-        # "devtools",
-        # "spp"),
-        repos="https://cloud.r-project.org/"))
+        "ggplot2",
+        "RColorBrewer",
+        "devtools",
+        "spp"),
+        repos="https://cloud.r-project.org/")
