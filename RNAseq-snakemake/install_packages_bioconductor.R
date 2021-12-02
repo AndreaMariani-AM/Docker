0a3f89d17d17d1498233dce63ee434c2f2f6a2ad
AndreaMariani-AM <mariani.andrea1@live.it>
2021-12-02 16:11:09 +0100

    trying bioc packages

--- RNAseq-snakemake/install_packages_bioconductor.R
+++ RNAseq-snakemake/install_packages_bioconductor.R
@@ -2,9 +2,9 @@
 
 BiocManager::install(c(
         'org.Mm.eg.db',
-        'org.Hs.eg.db',
-        'TxDb.Mmusculus.UCSC.mm9.knownGene',
-        'TxDb.Mmusculus.UCSC.mm10.knownGene',
-        'TxDb.Hsapiens.UCSC.hg19.knownGene',
-        'TxDb.Hsapiens.UCSC.hg18.knownGene',
+        # 'org.Hs.eg.db',
+        # 'TxDb.Mmusculus.UCSC.mm9.knownGene',
+        # 'TxDb.Mmusculus.UCSC.mm10.knownGene',
+        # 'TxDb.Hsapiens.UCSC.hg19.knownGene',
+        # 'TxDb.Hsapiens.UCSC.hg18.knownGene',
         'ChIPseeker'))
