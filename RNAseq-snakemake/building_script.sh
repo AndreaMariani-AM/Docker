75f6a9fe489a146e0ef5f37c576b4bd38dba82aa
AndreaMariani-AM <mariani.andrea1@live.it>
2021-12-02 16:46:50 +0100

    trying cran  packages

--- RNAseq-snakemake/building_script.sh
+++ RNAseq-snakemake/building_script.sh
@@ -6,7 +6,7 @@
 IMAGE_VERSION=$(date "+%m%d%y")
 
 # Build the image
-docker build --progress=plain --no-cache -t andreamariani/test_py37:${IMAGE_VERSION} -t andreamariani/test_py37:latest . > log.stdout
+docker build --progress=plain --no-cache -t andreamariani/test_py37:${IMAGE_VERSION} -t andreamariani/test_py37:latest .
 
 # docker build --no-cache -t andreamariani/rnaseq:${IMAGE_VERSION} -t andreamariani/rnaseq:latest . && \
 # docker push andreamariani/rnaseq:${IMAGE_VERSION}
