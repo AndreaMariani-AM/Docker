fcfc140038e741beaeb0e6def87cb97a5f577da7
AndreaMariani-AM <mariani.andrea1@live.it>
2021-12-03 15:08:09 +0100

    testing all bioc packages + cran YOLOOOOOOOOOOO

--- RNAseq-snakemake/install_packages_bioconductor.R
+++ RNAseq-snakemake/install_packages_bioconductor.R
@@ -2,9 +2,9 @@
 
 BiocManager::install(c(
         'ChIPseeker',
-        'org.Mm.eg.db'))
-        # 'org.Hs.eg.db',
-        # 'TxDb.Mmusculus.UCSC.mm9.knownGene',
-        # 'TxDb.Mmusculus.UCSC.mm10.knownGene',
-        # 'TxDb.Hsapiens.UCSC.hg19.knownGene',
-        # 'TxDb.Hsapiens.UCSC.hg18.knownGene'))
+        'org.Mm.eg.db',
+        'org.Hs.eg.db',
+        'TxDb.Mmusculus.UCSC.mm9.knownGene',
+        'TxDb.Mmusculus.UCSC.mm10.knownGene',
+        'TxDb.Hsapiens.UCSC.hg19.knownGene',
+        'TxDb.Hsapiens.UCSC.hg18.knownGene'))
