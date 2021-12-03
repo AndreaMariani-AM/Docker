28325d72533e3d2a23986d4ab7cd12682d90f44c
AndreaMariani-AM <mariani.andrea1@live.it>
2021-12-03 11:29:24 +0100

    tryint to fix Rsamtools dependencies for spp Cran package (the online one that i cant install)

--- RNAseq-snakemake/install_packages_bioconductor.R
+++ RNAseq-snakemake/install_packages_bioconductor.R
@@ -1,12 +1,10 @@
 # Bioconductor packages
 
 BiocManager::install(c(
-        "Rsamtools"
         # 'org.Mm.eg.db',
         # 'org.Hs.eg.db',
         # 'TxDb.Mmusculus.UCSC.mm9.knownGene',
         # 'TxDb.Mmusculus.UCSC.mm10.knownGene',
         # 'TxDb.Hsapiens.UCSC.hg19.knownGene',
         # 'TxDb.Hsapiens.UCSC.hg18.knownGene',
-        # 'ChIPseeker'
-        ))
+         'ChIPseeker'))
