73722ac290cba51243ebd7aa0db9bf1c46ebb1c3
AndreaMariani-AM <mariani.andrea1@live.it>
2021-12-06 10:48:51 +0100

    building the whole chipseq image

--- ChIPseq-snakemake/building_script.sh
+++ ChIPseq-snakemake/building_script.sh
@@ -6,5 +6,5 @@
 IMAGE_VERSION=$(date "+%m%d%y")
 
 # Build the image and push it to docker hub
-docker build --no-cache -t andreamariani/chipseq_snakemake:${IMAGE_VERSION} -t andreamariani/chipseq_snakemake:latest . && \
+docker build --progress=plain --no-cache -t andreamariani/chipseq_snakemake:${IMAGE_VERSION} -t andreamariani/chipseq_snakemake:latest . && \
 docker push andreamariani/chipseq_snakemake:${IMAGE_VERSION}
