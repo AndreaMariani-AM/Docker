3fd0e745e47515a99c937402b07fa2ceabcfb8b9
AndreaMariani-AM <mariani.andrea1@live.it>
2021-12-13 15:43:35 +0100

    testing packages utility

---
+++ RStudio/packages_compbio.R
@@ -0,0 +1,38 @@
+source('/Rscripts/install_packages_function.R')
+
+# packages for compbio
+packages_to_install <- c(
+  'bedr',
+  'biomaRt',
+  # 'BSgenome.Hsapiens.UCSC.hg19',
+  # 'BSgenome.Mmusculus.UCSC.mm10',
+  'ChIPseeker',
+  'clusterProfiler',
+  'DelayedArray',
+  'DelayedMatrixStats',
+  'DESeq2',
+  'DiffBind',
+  'enrichR',
+  # 'EnsDb.Hsapiens.v75',
+  # 'EnsDb.Mmusculus.v79',
+  'GenomicFeatures',
+  'GenomicRanges',
+  'gProfileR',
+  'gprofiler2',
+  'msigdbr',
+  'org.Hs.eg.db',
+  'org.Mm.eg.db',
+  'ReactomePA',
+  'rGREAT',
+  'Rmagic',
+  'Rsamtools',
+  'Rsubread',
+  'themis',
+  'topGO',
+  'TSCAN',
+  'TxDb.Hsapiens.UCSC.hg38.knownGene',
+  'TxDb.Mmusculus.UCSC.mm10.knownGene',
+  'tximport'
+)
+
+install_packages(packages_to_install)
---
+++ RStudio/packages_plotting.R
@@ -0,0 +1,45 @@
+source('/Rscripts/install_packages_function.R')
+
+# packages for plotting
+packages_to_install <- c(
+  'alluvial',
+  'beeswarm',
+  'circlize',
+  'ComplexHeatmap',
+  'cowplot',
+  'vankesteren/firatheme',
+  'chrisamiller/fishplot',
+  'ggalluvial',
+  'gganimate',
+  'ggbeeswarm',
+  'ggbio',
+  'ggdendro',
+  'ggExtra',
+  'ggforce',
+  'thomasp85/ggfx',
+  'erocoar/gghalves',
+  'ggpointdensity',
+  'ggpubr',
+  'ggraph',
+  'ggrastr',
+  'ggrepel',
+  'ggridges',
+  'ggsci',
+  'ggsignif',
+  'ggthemes',
+  'heatmaps',
+  'jcolors',
+  'paletteer',
+  'patchwork',
+  'pheatmap',
+  'plotly',
+  'prismatic',
+  'RColorBrewer',
+  'scico',
+  'UpSetR',
+  'VennDiagram',
+  'viridis',
+  'wesanderson'
+)
+
+install_packages(packages_to_install)
