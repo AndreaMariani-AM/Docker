9adbe7f08da2fd0e736ce51292b37961b91018b6
AndreaMariani-AM <mariani.andrea1@live.it>
2021-12-13 15:12:55 +0100

    testing until conda packages

--- RStudio/building_script.sh
+++ RStudio/building_script.sh
@@ -9,7 +9,7 @@ IMAGE_VERSION=$(date "+%m%d%y")
 ##===========================================##===========================================##
 # Build the image and push it to docker hub
 ##===========================================##===========================================##
-docker build --progress=plain --no-cache -t andreamariani/rstudio:${IMAGE_VERSION} -t andreamariani/rstudio:latest . 2>&1 | tee stdout.log #&& \
+#docker build --progress=plain --no-cache -t andreamariani/rstudio:${IMAGE_VERSION} -t andreamariani/rstudio:latest . 2>&1 | tee stdout.log #&& \
 # docker push andreamariani/rstudio:${IMAGE_VERSION}
 
 # USE THIS LINES ONLY WHEN EVERYTHING WORKS. Below is for testing.
@@ -18,7 +18,7 @@ docker build --progress=plain --no-cache -t andreamariani/rstudio:${IMAGE_VERSIO
 ##===========================================##===========================================##
 # Bulding/testing components of the image
 ##===========================================##===========================================##
-# docker build --progress=plain -t andreamariani/rstudo:${IMAGE_VERSION} -t andreamariani/rstudio:latest . 2>&1 | tee stdout.log
+docker build --progress=plain -t andreamariani/rstudo:${IMAGE_VERSION} -t andreamariani/rstudio:latest . 2>&1 | tee stdout.log
 
 # I think --no-cache arguments it's usefull when building the final image of when there are problmes withe dependencies,
 # otherwise it slows down everything. Last part redirects STDOUT/ERR both to STDOUT and a file that it's usefull to check
