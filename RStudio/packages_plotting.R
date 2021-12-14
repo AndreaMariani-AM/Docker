0936abf4aa8d271f5c2c507dabc1d1ba677c2bde
AndreaMariani-AM <mariani.andrea1@live.it>
2021-12-14 11:49:32 +0100

    having problems with S4Vectors package

--- RStudio/packages_plotting.R
+++ RStudio/packages_plotting.R
@@ -1,45 +1,50 @@
 source('/Rscripts/install_packages_function.R')
 
-# packages for plotting
-packages_to_install <- c(
-  'alluvial',
-  'beeswarm',
-  'circlize',
-  'ComplexHeatmap',
-  'cowplot',
-  'vankesteren/firatheme', #works
-  'chrisamiller/fishplot', #works
-  'ggalluvial',
-  'gganimate',
-  'ggbeeswarm',
-  'ggbio',
-  'ggdendro',
-  'ggExtra',
-  'ggforce',
-  'thomasp85/ggfx', #works
-  'erocoar/gghalves', #works
-  'ggpointdensity',
-  'ggpubr',
-  'ggraph',
-  'ggrastr',
-  'ggrepel',
-  'ggridges',
-  'ggsci',
-  'ggsignif',
-  'ggthemes',
-  'heatmaps',
-  'jcolors',
-  'paletteer',
-  'patchwork',
-  'pheatmap',
-  'plotly',
-  'prismatic',
-  'RColorBrewer',
-  'scico',
-  'UpSetR',
-  'VennDiagram',
-  'viridis',
-  'wesanderson'
-)
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
 
-install_packages(packages_to_install)
+BiocManager::install("S4Vectors")
+
+# packages for plotting
+# packages_to_install <- c(
+#   'alluvial',
+#   'beeswarm',
+#   'circlize',
+#   'ComplexHeatmap',
+#   'cowplot',
+#   'vankesteren/firatheme', #works
+#   'chrisamiller/fishplot', #works
+#   'ggalluvial',
+#   'gganimate',
+#   'ggbeeswarm',
+#   'ggbio',
+#   'ggdendro',
+#   'ggExtra',
+#   'ggforce',
+#   'thomasp85/ggfx', #works
+#   'erocoar/gghalves', #works
+#   'ggpointdensity',
+#   'ggpubr',
+#   'ggraph',
+#   'ggrastr',
+#   'ggrepel',
+#   'ggridges',
+#   'ggsci',
+#   'ggsignif',
+#   'ggthemes',
+#   'heatmaps',
+#   'jcolors',
+#   'paletteer',
+#   'patchwork',
+#   'pheatmap',
+#   'plotly',
+#   'prismatic',
+#   'RColorBrewer',
+#   'scico',
+#   'UpSetR',
+#   'VennDiagram',
+#   'viridis',
+#   'wesanderson'
+# )
+
+# install_packages(packages_to_install)
