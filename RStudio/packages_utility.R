c7663f05a1377e91d266237fa77640ecd62b4e9d
AndreaMariani-AM <mariani.andrea1@live.it>
2021-12-14 10:28:50 +0100

    options(build...) doesn't work, probably these packages have some issues, ignoring them. Build whole utility file

--- RStudio/packages_utility.R
+++ RStudio/packages_utility.R
@@ -1,59 +1,57 @@
-options(buildtools.check = function(action) TRUE)
-
-source('/Rscripts/install_packages_function.R')
+ source('/Rscripts/install_packages_function.R')
 
 # utility packages
 packages_to_install <- c(
   'remotes',
   'devtools',
-#   'tidyverse',
-   'thomasp85/ambient', #doesn't work
-#   'djnavarro/jasmines', #works
-   'jalvesaq/colorout', #doesn't work
-#   'concaveman',
-#   'corrr',
-#   'dendextend',
-#   'densityClust',
-   'xiaodaigh/disk.frame' # doens't work
-#   'DT',
-#   'fastcluster',
-#   'FlowSOM',
-#   'foreign',
-#   'future.apply',
-#   'gam',
-#   'glue',
-#   'golem',
-#   'here',
-#   'h2o',
-#   'hdf5r',
-#   'intrinsicDimension',
-#   'lwgeom',
-#   'magrittr',
-#   'openxlsx',
-#   'phylogram',
-#   'pryr',
-#   'renv',
-#   'reshape2',
-#   'reticulate',
-#   'rgdal',
-#   'roxygen2',
-#   'Rtsne',
-#   'scales',
-#   'shiny',
-#   'shinydashboard',
-#   'shinyjs',
-#   'shinythemes',
-#   'sf',
-#   'statsbomb/StatsBombR', #works
-#   'stringi',
-#   'tensorflow',
-#   'tsne',
-#   'krlmlr/ulimit',
-#   'umap',
-#   'uwot',
-#   'workflowr',
-#   'XML',
-#   'xml2'
+  'tidyverse',
+ # 'thomasp85/ambient', #doesn't work
+  'djnavarro/jasmines', #works
+ # 'jalvesaq/colorout', #doesn't work
+  'concaveman',
+  'corrr',
+  'dendextend',
+  'densityClust',
+ # 'xiaodaigh/disk.frame' # doens't work
+  'DT',
+  'fastcluster',
+  'FlowSOM',
+  'foreign',
+  'future.apply',
+  'gam',
+  'glue',
+  'golem',
+  'here',
+  'h2o',
+  'hdf5r',
+  'intrinsicDimension',
+  'lwgeom',
+  'magrittr',
+  'openxlsx',
+  'phylogram',
+  'pryr',
+  'renv',
+  'reshape2',
+  'reticulate',
+  'rgdal',
+  'roxygen2',
+  'Rtsne',
+  'scales',
+  'shiny',
+  'shinydashboard',
+  'shinyjs',
+  'shinythemes',
+  'sf',
+  'statsbomb/StatsBombR', #works
+  'stringi',
+  'tensorflow',
+  'tsne',
+  'krlmlr/ulimit',
+  'umap',
+  'uwot',
+  'workflowr',
+  'XML',
+  'xml2'
 )
 
 install_packages(packages_to_install)
