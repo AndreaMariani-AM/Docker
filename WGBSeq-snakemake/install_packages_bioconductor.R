19532931a22fcefc4b867bcbec8c1d5c30a742a3
AndreaMariani-AM <mariani.andrea1@live.it>
2022-05-26 10:23:38 +0200

    adding a new docker image repo for WGBSeq

---
+++ WGBSeq-snakemake/install_packages_bioconductor.R
@@ -0,0 +1,14 @@
+# Bioconductor packages
+
+BiocManager::install(c(
+        'DESeq2',
+        'org.Mm.eg.db',
+        'org.Hs.eg.db',
+        'clusterProfiler',
+        'pcaExplorer',
+        'ReactomePA',
+        'IHW',
+        'apeglm',
+        'tximport',
+        'rhdf5',
+        'GenomicFeatures'))
---
+++ WGBSeq-snakemake/install_packages_cran.R
@@ -0,0 +1,17 @@
+# Cran packages
+
+install.packages(c(
+        "data.table",
+        "RColorBrewer",
+        "devtools",
+        "tidyverse",
+        "tibble",
+        "openxlsx",
+        "msigdbr",
+        "ggforce"),
+        repos="https://cloud.r-project.org/")
+
+# Devtools pkg
+
+devtools::install_github(c(
+         "VPetukhov/ggrastr"))
